da7a0cfcfcd9aec2dc79b83fd04c3c5f49a2c6b7
Todd Matthews <v-Todd_Matthews@carmax.com>
2020-04-15 11:20:44 -0400

    Add card component

--- src/users/components/UserItem.tsx
+++ src/users/components/UserItem.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-import Avatar from '../../shared/components/UIElements/Avatar'
+import Avatar from "../../shared/components/UIElements/Avatar";
+import Card from "../../shared/components/UIElements/Card";
 import "./UserItem.css";
+import { Link } from "react-router-dom";
 
 interface IUserItemProps {
   key: string;
@@ -10,17 +12,23 @@ interface IUserItemProps {
   recipeCount: number;
 }
 export default function UserItem(props: IUserItemProps) {
+  const { id, image, name, recipeCount } = props;
+
   return (
     <li className="user-item">
-      <div className="user-item__image">
-        <Avatar image={props.image} altText="user avatar"/>
-      </div>
-      <div className="user-item__info">
-        <h2>{props.name}</h2>
-        <h3>
-          {props.recipeCount} {props.recipeCount === 1 ? "Recipe" : "Recipes"}
-        </h3>
-      </div>
+      <Card className="user-item__content">
+        <Link to={`/${id}/recipes`}>
+          <div className="user-item__image">
+            <Avatar image={image} altText="user avatar" />
+          </div>
+          <div className="user-item__info">
+            <h2>{name}</h2>
+            <h3>
+              {recipeCount} {recipeCount === 1 ? "Recipe" : "Recipes"}
+            </h3>
+          </div>
+        </Link>
+      </Card>
     </li>
   );
 }
